1fb78d248f08c2498e1a8a87c62ad04204947ac4
liuzhaoluliuzhaolu <liuzhaoluliuzhaolu@didiglobal.com>
2020-11-03 17:17:09 +0800

    tomcat运行

--- src/main/java/com/zero/qinqiong/QinqiongApplication.java
+++ src/main/java/com/zero/qinqiong/QinqiongApplication.java
@@ -2,10 +2,16 @@ package com.zero.qinqiong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QinqiongApplication {
+public class QinqiongApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(QinqiongApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(QinqiongApplication.class, args);
     }
